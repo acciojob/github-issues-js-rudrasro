e0d3f6302cceb6c6a617e49686de02c3e197e113
Supriyo Rudra <117836404+rudrasro@users.noreply.github.com>
2023-02-11 22:30:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
-let pageNumber = 1;
+let currentPage = 1;
+let totalPages;
 
-const loadIssues = async (pageNumber) => {
-  const res = await fetch(`https://api.github.com/repositories/1296269/issues?page=${pageNumber}&per_page=5`);
-  const data = await res.json();
+async function fetchIssues(pageNumber) {
+  const response = await fetch(
+    `https://api.github.com/repositories/1296269/issues?page=${pageNumber}&per_page=5`
+  );
+  const data = await response.json();
+  return data;
+}
 
-  const issueList = document.getElementById("issue-list");
-  issueList.innerHTML = "";
-
-  data.forEach((issue) => {
-    const issueItem = document.createElement("li");
-    issueItem.innerText = issue.title;
-    issueList.appendChild(issueItem);
-  });
-};
-
-const updatePage = () => {
-  const pageHeading = document.getElementById("page-number");
-  pageHeading.innerText = `Page number ${pageNumber}`;
-
-  loadIssues(pageNumber);
-};
-
-document.getElementById("load_next").addEventListener("click", () => {
-  pageNumber += 1;
-  updatePage();
-});
-
-document.getElementById("load_prev").addEventListener("click", () => {
-  if (pageNumber > 1) {
-    pageNumber -= 1;
-    updatePage();
+async function updatePage(pageNumber) {
+  const issues = await fetchIssues(pageNumber);
+  totalPages = Math.ceil(issues.length / 5);
+  document.getElementById("page_number").innerText = `Page Number ${pageNumber}`;
+  let issueList = "";
+  for (let i = 0; i < 5; i++) {
+    if (issues[i]) {
+      issueList += `<li>${issues[i].title}</li>`;
+    }
   }
+  document.getElementById("issues_list").innerHTML = issueList;
+}
+
+document.getElementById("load_next").addEventListener("click", function () {
+  if (currentPage === totalPages) return;
+  currentPage++;
+  updatePage(currentPage);
 });
 
-updatePage();
+document.getElementById("load_prev").addEventListener("click", function () {
+  if (currentPage === 1) return;
+  currentPage--;
+  updatePage(currentPage);
+});
+
+updatePage(currentPage);
