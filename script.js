0d0eb4ae7d4541265fc455bb524a35ee4b52eaf4
Supriyo Rudra <117836404+rudrasro@users.noreply.github.com>
2023-02-11 23:17:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,30 @@
-let pageNumber = 1;
-let issuesList = document.getElementById("issue_list");
-let pageHeading = document.getElementById("page_number");
+let pageNumber = 0;
 
-document.getElementById("load_next").addEventListener("click", function () {
+document.getElementById("load_next").addEventListener("click", function() {
   pageNumber++;
+  updatePage(pageNumber);
+});
+
+document.getElementById("load_prev").addEventListener("click", function() {
+  if (pageNumber > 1) {
+    pageNumber--;
+    updatePage(pageNumber);
+  }
+});
+
+function updatePage(pageNumber) {
   fetch(`https://api.github.com/repositories/1296269/issues?page=${pageNumber}&per_page=5`)
     .then(response => response.json())
     .then(data => {
-      issuesList.innerHTML = "";
-      data.forEach(issue => {
-        let li = document.createElement("li");
-        li.innerText = issue.title;
-        issuesList.appendChild(li);
+      let issueList = document.getElementById("issue_list");
+      issueList.innerHTML = "";
+      data.forEach(function(issue) {
+        let listItem = document.createElement("li");
+        listItem.innerText = issue.title;
+        issueList.appendChild(listItem);
       });
-      pageHeading.innerText = `Page number ${pageNumber}`;
+      document.getElementById("page_number").innerText = `Page Number ${pageNumber}`;
     });
-});
+}
 
-document.getElementById("load_prev").addEventListener("click", function () {
-  if (pageNumber > 1) {
-    pageNumber--;
-    fetch(`https://api.github.com/repositories/1296269/issues?page=${pageNumber}&per_page=5`)
-      .then(response => response.json())
-      .then(data => {
-        issuesList.innerHTML = "";
-        data.forEach(issue => {
-          let li = document.createElement("li");
-          li.innerText = issue.title;
-          issuesList.appendChild(li);
-        });
-        pageHeading.innerText = `Page number ${pageNumber}`;
-      });
-  }
-});
+updatePage(pageNumber);
