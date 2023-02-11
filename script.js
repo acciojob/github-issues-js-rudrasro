4e41e153474fb2cdcdd20f6e55b49ec5f8cc0027
Supriyo Rudra <117836404+rudrasro@users.noreply.github.com>
2023-02-11 22:39:44 +0530

    update

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
-let currentPage = 1;
-let totalPages;
+const issuesList = document.getElementById("issue-list");
+const pageNumberEl = document.getElementById("page-number");
+const loadNextBtn = document.getElementById("load-next");
+const loadPrevBtn = document.getElementById("load-prev");
 
-async function fetchIssues(pageNumber) {
-  const response = await fetch(
-    `https://api.github.com/repositories/1296269/issues?page=${pageNumber}&per_page=5`
-  );
-  const data = await response.json();
-  return data;
-}
+let pageNumber = 1;
 
-async function updatePage(pageNumber) {
-  const issues = await fetchIssues(pageNumber);
-  totalPages = Math.ceil(issues.length / 5);
-  document.getElementById("page_number").innerText = `Page Number ${pageNumber}`;
-  let issueList = "";
-  for (let i = 0; i < 5; i++) {
-    if (issues[i]) {
-      issueList += `<li>${issues[i].title}</li>`;
-    }
+const updatePage = (issues) => {
+  // Clear the current list of issues
+  issuesList.innerHTML = "";
+
+  // Loop through the issues and create a new list item for each one
+  for (let issue of issues) {
+    const newItem = document.createElement("li");
+    newItem.innerText = issue.title;
+    issuesList.appendChild(newItem);
   }
-  document.getElementById("issues_list").innerHTML = issueList;
-}
 
-document.getElementById("load_next").addEventListener("click", function () {
-  if (currentPage === totalPages) return;
-  currentPage++;
-  updatePage(currentPage);
+  // Update the page number
+  pageNumberEl.innerText = `Page Number ${pageNumber}`;
+};
+
+const loadIssues = (page) => {
+  fetch(`https://api.github.com/repositories/1296269/issues?page=${page}&per_page=5`)
+    .then((response) => response.json())
+    .then((data) => updatePage(data));
+};
+
+loadIssues(pageNumber);
+
+loadNextBtn.addEventListener("click", () => {
+  pageNumber++;
+  loadIssues(pageNumber);
 });
 
-document.getElementById("load_prev").addEventListener("click", function () {
-  if (currentPage === 1) return;
-  currentPage--;
-  updatePage(currentPage);
+loadPrevBtn.addEventListener("click", () => {
+  if (pageNumber > 1) {
+    pageNumber--;
+    loadIssues(pageNumber);
+  }
 });
-
-updatePage(currentPage);
