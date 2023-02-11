1371ade5ef3284ccafbbcfccf3637e7021a60019
Supriyo Rudra <117836404+rudrasro@users.noreply.github.com>
2023-02-11 22:26:57 +0530

    update

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-const issuesList = document.getElementById("issues-list");
-const pageNumber = document.getElementById("page-number");
-const loadPrevButton = document.getElementById("load_prev");
-const loadNextButton = document.getElementById("load_next");
+let pageNumber = 1;
 
-let currentPage = 1;
+const loadIssues = async (pageNumber) => {
+  const res = await fetch(`https://api.github.com/repositories/1296269/issues?page=${pageNumber}&per_page=5`);
+  const data = await res.json();
 
-loadNextButton.addEventListener("click", () => {
-  currentPage += 1;
-  fetchIssues(currentPage);
+  const issueList = document.getElementById("issue-list");
+  issueList.innerHTML = "";
+
+  data.forEach((issue) => {
+    const issueItem = document.createElement("li");
+    issueItem.innerText = issue.title;
+    issueList.appendChild(issueItem);
+  });
+};
+
+const updatePage = () => {
+  const pageHeading = document.getElementById("page-number");
+  pageHeading.innerText = `Page number ${pageNumber}`;
+
+  loadIssues(pageNumber);
+};
+
+document.getElementById("load_next").addEventListener("click", () => {
+  pageNumber += 1;
+  updatePage();
 });
 
-loadPrevButton.addEventListener("click", () => {
-  if (currentPage > 1) {
-    currentPage -= 1;
-    fetchIssues(currentPage);
+document.getElementById("load_prev").addEventListener("click", () => {
+  if (pageNumber > 1) {
+    pageNumber -= 1;
+    updatePage();
   }
 });
 
-async function fetchIssues(page) {
-  const response = await fetch(
-    `https://api.github.com/repositories/1296269/issues?page=${page}&per_page=5`
-  );
-  const data = await response.json();
-
-  pageNumber.innerHTML = `Page number ${page}`;
-  issuesList.innerHTML = "";
-
-  data.forEach((issue) => {
-    const listItem = document.createElement("li");
-    listItem.innerHTML = issue.title;
-    issuesList.appendChild(listItem);
-  });
-}
-
-fetchIssues(currentPage);
+updatePage();
